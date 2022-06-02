48f5ae3eec89550aad2683e255339cc6279d74d8
Gonzalo Juarez <Juarezgonzalo000@gmail.com>
2022-06-02 16:43:38 -0300

    pushed changes

--- server/src/models/Task.js
+++ server/src/models/Task.js
@@ -4,8 +4,14 @@ const {model, Schema} = require('mongoose');
 const Task = new Schema({
     title : {type:String, required: true},
     description : {type: String, required:true},
-    create_date : {type: Date, default: new Date()}
+    create_date : {type: Date, default: new Date()},
+    todoStatus: { type: Boolean, default: false},
 })
+
+Task.methods.seCompleto = function () {
+    return !this.todoStatus
+}
+
 
 
 module.exports = model("Task", Task)
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,9 +1,20 @@
 const {model, Schema} = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const User = new Schema({
     name : { type : String, required:true},
     email : {type : String, required:true},
     password : {type: String, required:true},
 })
+
+User.methods.PasswordEncrypt = async password => {
+    const minim = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, minim);
+}
+
+User.methods.comparePassword = function(password){
+    return bcrypt.compare(password , this.password)
+}
+
 
 module.exports = model("Usuario", User);
